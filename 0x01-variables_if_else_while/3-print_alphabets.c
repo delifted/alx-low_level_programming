87ecc81b9bf9f33b18059e54e9796746faefe7bd
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2022-12-12 14:55:19 -0800

    Tasks

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,15 @@
  *
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char a;
 
-	for (a = 'a'; a <= 'z'; a++)
+	for(a = 'a'; a <= 'z'; a++)
 		putchar (a);
-	for (a = 'A'; a <= 'Z'; a++)
-		putchar (a);
-	putchar ('\n');
+	for(a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
 
-	return (0);
+	return(0);
 }
