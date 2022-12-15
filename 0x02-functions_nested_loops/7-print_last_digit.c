682671ff9c2408e273768689e9af24dce2639bec
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2022-12-14 20:57:33 -0800

    Updated

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n: the passed arguments
+ * Return: return the last digit
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+		x = -1 * (n % 10);
+	else
+		x = n % 10;
+	_putchar((x % 10) + '0');
+	return (x % 10);
+}
