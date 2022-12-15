a31f862c3e68f1c122d4c8188061ddfe9b1e820a
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2022-12-14 20:33:29 -0800

    Alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> prints the lower case alphabets
+ */
+void print_alphabet_x10(void)
+{
+	int y;
+	int x;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+
+}
