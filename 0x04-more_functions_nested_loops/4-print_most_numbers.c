f3ddf42c1d3f9617ab24fa4162b682d2cd88b368
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2022-12-16 23:36:11 -0800

    Missing numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "_putchar.c"
 
 /**
  * print_most_numbers - print numbers from 0 - 9 except 2 and 4
@@ -6,13 +7,14 @@
  * Return: void
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	char c;
-	for (c = 0; c <= 9; c++)
+
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == 2 || c == 4))
+		if (!(c == '2' || c == '4'))
 		_putchar(c);
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
