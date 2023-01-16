1018d27243cc3755a32a0b9db07e2ef1c4bb73ec
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-01-16 02:32:14 +0000

    Task 1 solved

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - this is a function to print its name
+ * main - this is a function to print number of arguments passed to it.
  * @argc: argc parameter
  * @argv: an array of command listed
  * Return: 0 for success
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
