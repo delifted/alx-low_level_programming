502abfcb3fb7e8e60f89e548d781e407f808a134
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-01-16 02:47:26 +0000

    Second Task finished

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function to print number of arguments passed to it.
+ * @argc: argc parameter
+ * @argv: an array of command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv index %d is = %s\n", i, argv[i]);
+	}
+	return (0);
+}
