656c86f6b38bb4c0a5cc5bec803b54e53d0a61c7
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2023-01-28 00:28:14 +0000

    Task 1 Corrected

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (array && action)
-		for (i = o; i < size; i++)
+		for (i = 0; i < size; i++)
 			action(array[i]);
 }
